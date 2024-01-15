71b77afb2258394fb3920c9f8e9ceee06654030f
Ronak Kothari <ronakkothari301@gmail.com>
2024-01-15 12:01:40 +0530

    adding vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import Main from "./components/Main";
 import TimeLine from "./components/TimeLine";
 import ContactUs from "./components/ContactUs";
 import AboutUs from "./components/AboutUs";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <main>
+      <Analytics />
       <Navbar />
       <HomePage />
       <Main />
